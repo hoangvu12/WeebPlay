5ccf07a75981b8fcc46d57ecac46475d899832cc
hoangvu12 <68330291+hoangvu12@users.noreply.github.com>
2021-05-13 08:01:05 +0700

    refactor(watch): code splitting

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,18 +1,20 @@
+import { useEffect, useRef, useState } from "react";
 import { Transition } from "@headlessui/react";
 import { ThumbDownIcon, ThumbUpIcon } from "@heroicons/react/solid";
-import classNames from "classnames";
-import React, { useEffect, useRef, useState } from "react";
-import Linkify from "react-linkify";
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
+
+import classNames from "classnames";
+import Linkify from "react-linkify";
 import videojs from "video.js";
-import "videojs-max-quality-selector/dist/videojs-max-quality-selector.css";
 
 import Video from "../components/common/Video";
 import Item from "../components/Item";
 import useQuery from "../hooks/useQuery";
 import { isEmpty, numberWithCommas } from "../utils";
 import YouTubeAPI from "../utils/youtube";
+
+import "videojs-max-quality-selector/dist/videojs-max-quality-selector.css";
 
 const yt = new YouTubeAPI();
 
@@ -107,7 +109,8 @@ const Watch = () => {
     };
 
     const parsePlaylistId = (data) => {
-      const expression = /[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)?/gi;
+      const expression =
+        /[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)?/gi;
       const regex = new RegExp(expression);
 
       const urls = data.match(regex);
@@ -132,9 +135,9 @@ const Watch = () => {
       if (!playlistId) {
         const parsedPlaylistId = parsePlaylistId(info.description);
 
-        history.replace(
-          `/watch?videoId=${videoId}&playlistId=${parsedPlaylistId}`
-        );
+        const href = `/watch?videoId=${videoId}&playlistId=${parsedPlaylistId}`;
+
+        history.replace(href);
 
         return;
       }
@@ -168,7 +171,6 @@ const Watch = () => {
     });
 
     getData();
-    // updateVideoTime();
 
     return () => {
       player && player.off("timeupdate");
@@ -199,92 +201,27 @@ const Watch = () => {
               !isEmpty(playlistItems) ? "md:grid-cols-5" : "md:grid-cols-1"
             )}
           >
-            <div className="md:col-span-3 mb-5 border-r px-2 md:border-gray-200 md:border-opacity-20">
-              <div className="flex justify-between items-center w-full">
-                <p className="text-white text-lg font-medium">
-                  {videoInfo.title}
-                </p>
-              </div>
+            <div className="md:col-span-3 mb-5 px-2">
+              <p className="text-white text-lg font-medium">
+                {videoInfo.title}
+              </p>
+
               <div className="flex justify-between items-center text-gray-400 mb-5">
-                <div className="flex">
-                  <p className="text-base mr-2">
-                    {numberWithCommas(videoInfo.statistics.viewCount)} lượt xem
-                  </p>
-                  <p className="text-base mr-2">•</p>
-
-                  <p className="text-base">
-                    {new Date(videoInfo.publishedAt).toLocaleDateString(
-                      "vi-VN"
-                    )}
-                  </p>
-                </div>
-
-                <div className="flex items-center justify-between text-gray-300">
-                  <div className="flex mr-4">
-                    <ThumbUpIcon className="h-6 w-6 block mr-1" />
-
-                    <p className="text-base">
-                      {numberWithCommas(videoInfo.statistics.likeCount)}
-                    </p>
-                  </div>
-                  <div className="flex">
-                    <ThumbDownIcon className="h-6 w-6 block mr-1" />
-                    <p className="text-base">
-                      {numberWithCommas(videoInfo.statistics.dislikeCount)}
-                    </p>
-                  </div>
-                </div>
+                <Statistics
+                  statistics={videoInfo.statistics}
+                  publishedAt={videoInfo.publishedAt}
+                />
               </div>
 
-              <div>
-                <p
-                  style={{ whiteSpace: "pre-wrap" }}
-                  className="text-sm text-white"
-                >
-                  <Linkify
-                    componentDecorator={(decoratedHref, decoratedText, key) => {
-                      if (
-                        decoratedHref.includes(
-                          "https://www.youtube.com/playlist?list="
-                        )
-                      ) {
-                        return (
-                          <Link
-                            to={`/playlist?id=${playlistId}`}
-                            key={key}
-                            className="text-blue-400"
-                          >
-                            Tại đây
-                          </Link>
-                        );
-                      }
-
-                      return (
-                        <a
-                          target="blank"
-                          href={decoratedHref}
-                          key={key}
-                          className="line-clamp-1 text-blue-400"
-                        >
-                          {decoratedText}
-                        </a>
-                      );
-                    }}
-                  >
-                    {videoInfo.description}
-                  </Linkify>
-                </p>
-              </div>
+              <Description
+                description={videoInfo.description}
+                playlistId={playlistId}
+              />
             </div>
+
             {playlistId && (
               <div className="md:col-span-2 p-2">
-                {playlistItems.map((item, index) => (
-                  <Item
-                    {...item}
-                    key={index}
-                    active={item.resourceId.videoId === videoId}
-                  />
-                ))}
+                <VideoItems items={playlistItems} videoId={videoId} />
               </div>
             )}
           </div>
@@ -293,5 +230,82 @@ const Watch = () => {
     </div>
   );
 };
+
+function Statistics({ publishedAt, statistics }) {
+  return (
+    <>
+      <div className="flex">
+        <p className="text-base mr-2">
+          {numberWithCommas(statistics.viewCount)} lượt xem
+        </p>
+
+        <p className="text-base mr-2">•</p>
+
+        <p className="text-base">
+          {new Date(publishedAt).toLocaleDateString("vi-VN")}
+        </p>
+      </div>
+
+      <div className="flex items-center justify-between text-gray-300">
+        <div className="flex mr-4">
+          <ThumbUpIcon className="h-6 w-6 block mr-1" />
+
+          <p className="text-base">{numberWithCommas(statistics.likeCount)}</p>
+        </div>
+        <div className="flex">
+          <ThumbDownIcon className="h-6 w-6 block mr-1" />
+          <p className="text-base">
+            {numberWithCommas(statistics.dislikeCount)}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function VideoItems(props) {
+  return props.items.map((item, index) => (
+    <Item
+      {...item}
+      key={index}
+      active={item.resourceId.videoId === props.videoId}
+    />
+  ));
+}
+
+function Description(props) {
+  return (
+    <p style={{ whiteSpace: "pre-wrap" }} className="text-sm text-white">
+      <Linkify
+        componentDecorator={(decoratedHref, decoratedText, key) => {
+          if (decoratedHref.includes("youtube.com/playlist")) {
+            return (
+              <Link
+                to={`/playlist?id=${props.playlistId}`}
+                key={key}
+                className="text-blue-400"
+              >
+                Tại đây
+              </Link>
+            );
+          }
+
+          return (
+            <a
+              target="blank"
+              href={decoratedHref}
+              key={key}
+              className="line-clamp-1 text-blue-400"
+            >
+              {decoratedText}
+            </a>
+          );
+        }}
+      >
+        {props.description}
+      </Linkify>
+    </p>
+  );
+}
 
 export default Watch;
